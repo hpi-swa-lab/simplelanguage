78a4c63f9116089a3c857b1e07f350b5f52a8ecd
Philipp Otto <philipp.otto2@student.hpi.de>
2018-06-25 12:40:38 +0200

    Implement counting of subshapes at specific positions

--- language/src/main/java/com/oracle/truffle/sl/builtins/SLConsCellBuiltin.java
+++ language/src/main/java/com/oracle/truffle/sl/builtins/SLConsCellBuiltin.java
@@ -1,5 +1,8 @@
 package com.oracle.truffle.sl.builtins;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import com.oracle.truffle.api.object.*;
@@ -12,14 +15,42 @@ import com.oracle.truffle.sl.runtime.SLObjectType;
 public abstract class SLConsCellBuiltin extends SLBuiltinNode {
 
     private static final Layout LAYOUT = Layout.createLayout();
+    private static final Shape.Allocator ALLOCATOR = LAYOUT.createAllocator();
+    private static final Location headLocation = ALLOCATOR.locationForType(Object.class);
+    private static final Location tailLocation = ALLOCATOR.locationForType(Object.class);
+    private static final Shape emptyShape = LAYOUT.createShape(SLObjectType.SINGLETON);
+
+    private static Map<Integer, Map<Shape, Integer>> observedShapes = new HashMap<>();
+
+    static {
+        observedShapes.put(0, new HashMap<>());
+        observedShapes.put(1, new HashMap<>());
+    }
+
+    private static void observeShape(Integer position, Shape shape) {
+        Integer numberObserved = observedShapes.get(position).getOrDefault(shape, 0);
+        numberObserved += 1;
+        observedShapes.get(position).put(shape, numberObserved);
+
+        if (numberObserved == 7) {
+            System.out.println("Transformation recorded at position\n" + 
+                position.toString() + " contains\n" + shape.toString());
+        }
+    }
 
     @Specialization
     public final DynamicObject newCell(Object head, Object tail) {
-        Shape.Allocator allocator = LAYOUT.createAllocator();
-        Location headLocation = allocator.locationForType(Integer.class);
-        Location tailLocation = allocator.locationForType(Object.class);
+        if (head instanceof DynamicObject) {
+            DynamicObject headDynamic = (DynamicObject)head;
+            observeShape(0, headDynamic.getShape());
+        }
+        
+        if (tail instanceof DynamicObject) {
+            DynamicObject tailDynamic = (DynamicObject)tail;
+            observeShape(1, tailDynamic.getShape());
+        }
 
-        Shape shape = LAYOUT.createShape(SLObjectType.SINGLETON)
+        Shape shape = emptyShape
                 .addProperty(
                         Property.create("head", headLocation, 0)
                 )
@@ -31,5 +62,4 @@ public abstract class SLConsCellBuiltin extends SLBuiltinNode {
         cell.set("tail", tail);
         return cell;
     }
-
 }

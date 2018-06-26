97190aeab519b29fd7866f1d32031881f8b9bb81
Philipp Otto <philipp.otto2@student.hpi.de>
2018-06-26 11:46:43 +0200

    WIP state of inlining (only shapes are inlined, not values so far)

--- language/src/main/java/com/oracle/truffle/sl/builtins/SLConsCellBuiltin.java
+++ language/src/main/java/com/oracle/truffle/sl/builtins/SLConsCellBuiltin.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.sl.runtime.*;
 import com.oracle.truffle.api.object.*;
 import com.oracle.truffle.sl.runtime.SLObjectType;
 
@@ -20,46 +21,44 @@ public abstract class SLConsCellBuiltin extends SLBuiltinNode {
     private static final Location tailLocation = ALLOCATOR.locationForType(Object.class);
     private static final Shape emptyShape = LAYOUT.createShape(SLObjectType.SINGLETON);
 
-    private static Map<Integer, Map<Shape, Integer>> observedShapes = new HashMap<>();
-
-    static {
-        observedShapes.put(0, new HashMap<>());
-        observedShapes.put(1, new HashMap<>());
-    }
-
-    private static void observeShape(Integer position, Shape shape) {
-        Integer numberObserved = observedShapes.get(position).getOrDefault(shape, 0);
-        numberObserved += 1;
-        observedShapes.get(position).put(shape, numberObserved);
-
-        if (numberObserved == 7) {
-            System.out.println("Transformation recorded at position\n" + 
-                position.toString() + " contains\n" + shape.toString());
-        }
-    }
-
     @Specialization
     public final DynamicObject newCell(Object head, Object tail) {
+
+        Property headProperty = Property.create("head", headLocation, 0);
+        Property tailProperty = Property.create("tail", headLocation, 0);
+        Shape shape = emptyShape
+                .addProperty(headProperty)
+                .addProperty(tailProperty);
+
+        SLShapeWrapper wrapper = SLShapeWrapper.getWrapperForShape(shape);
+
         if (head instanceof DynamicObject) {
             DynamicObject headDynamic = (DynamicObject)head;
-            observeShape(0, headDynamic.getShape());
-        }
-        
-        if (tail instanceof DynamicObject) {
-            DynamicObject tailDynamic = (DynamicObject)tail;
-            observeShape(1, tailDynamic.getShape());
+            Shape headSubshape = headDynamic.getShape();
+            wrapper.observeSubshape(0, headSubshape);
+            if (wrapper.isTransformation(0, headSubshape)) {
+                shape = shape.removeProperty(headProperty); 
+                shape = SLShapeWrapper.inlineSubshape(shape, headSubshape, 0, "head");
+            }
         }
 
-        Shape shape = emptyShape
-                .addProperty(
-                        Property.create("head", headLocation, 0)
-                )
-                .addProperty(
-                        Property.create("tail", tailLocation, 0));
+        if (tail instanceof DynamicObject) {
+            DynamicObject tailDynamic = (DynamicObject)tail;
+            Shape tailSubshape = tailDynamic.getShape();
+            wrapper.observeSubshape(1, tailSubshape);
+            if (wrapper.isTransformation(1, tailSubshape)) {
+                shape = shape.removeProperty(tailProperty); 
+                shape = SLShapeWrapper.inlineSubshape(shape, tailSubshape, 1, "tail");
+            }
+        }
+
+        // Shape may have changed due to tranformations
+        wrapper = SLShapeWrapper.getWrapperForShape(shape);
 
         DynamicObject cell = shape.newInstance();
         cell.set("head", head);
         cell.set("tail", tail);
+        System.out.println("Object shape: " + cell.getShape());
         return cell;
     }
 }
